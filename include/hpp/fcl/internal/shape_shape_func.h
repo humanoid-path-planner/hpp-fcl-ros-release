fde298bd780abd81ef47d193a4f1f17c724c9744
Wolfgang Merkt <w.merkt@gmail.com>
2022-08-01 12:51:24 +0100

    Rebase from 'debian/melodic/hpp-fcl'

--- include/hpp/fcl/internal/shape_shape_func.h
+++ include/hpp/fcl/internal/shape_shape_func.h
@@ -40,74 +40,126 @@
 /// @cond INTERNAL
 
 #include <hpp/fcl/collision_data.h>
+#include <hpp/fcl/collision_utility.h>
 #include <hpp/fcl/narrowphase/narrowphase.h>
+#include <hpp/fcl/shape/geometric_shapes_traits.h>
 
-namespace hpp
-{
-namespace fcl
-{
-  template<typename T_SH1, typename T_SH2>
-  HPP_FCL_DLLAPI FCL_REAL ShapeShapeDistance
-    (const CollisionGeometry* o1, const Transform3f& tf1,
-     const CollisionGeometry* o2, const Transform3f& tf2,
-     const GJKSolver* nsolver, const DistanceRequest& request,
-     DistanceResult& result);
+namespace hpp {
+namespace fcl {
 
-  template<typename T_SH1, typename T_SH2>
-  HPP_FCL_DLLAPI std::size_t ShapeShapeCollide
-    (const CollisionGeometry* o1, const Transform3f& tf1,
-     const CollisionGeometry* o2, const Transform3f& tf2, 
-     const GJKSolver* nsolver, const CollisionRequest& request,
-     CollisionResult& result);
+template <typename T_SH1, typename T_SH2>
+HPP_FCL_DLLAPI FCL_REAL ShapeShapeDistance(const CollisionGeometry* o1,
+                                           const Transform3f& tf1,
+                                           const CollisionGeometry* o2,
+                                           const Transform3f& tf2,
+                                           const GJKSolver* nsolver,
+                                           const DistanceRequest& request,
+                                           DistanceResult& result);
 
-#define SHAPE_SHAPE_DISTANCE_SPECIALIZATION(T1,T2) \
-template<> \
-HPP_FCL_DLLAPI FCL_REAL ShapeShapeDistance<T1,T2> \
-  (const CollisionGeometry* o1, const Transform3f& tf1, \
-   const CollisionGeometry* o2, const Transform3f& tf2, \
-   const GJKSolver* nsolver, const DistanceRequest& request, \
-   DistanceResult& result); \
-template<> \
-HPP_FCL_DLLAPI FCL_REAL ShapeShapeDistance<T2,T1> \
-(const CollisionGeometry* o1, const Transform3f& tf1, \
- const CollisionGeometry* o2, const Transform3f& tf2, \
- const GJKSolver* nsolver, const DistanceRequest& request, \
- DistanceResult& result)
+template <typename T_SH1, typename T_SH2>
+struct ShapeShapeCollider {
+  static std::size_t run(const CollisionGeometry* o1, const Transform3f& tf1,
+                         const CollisionGeometry* o2, const Transform3f& tf2,
+                         const GJKSolver* nsolver,
+                         const CollisionRequest& request,
+                         CollisionResult& result) {
+    if (request.isSatisfied(result)) return result.numContacts();
 
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Box,Halfspace);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Box,Plane);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Box,Sphere);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Capsule,Capsule);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Capsule,Halfspace);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Capsule,Plane);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cone,Halfspace);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cone,Plane);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cylinder,Halfspace);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cylinder,Plane);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere,Halfspace);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere,Plane);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere,Sphere);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere,Cylinder);
+    DistanceResult distanceResult;
+    DistanceRequest distanceRequest(request.enable_contact);
+    FCL_REAL distance = ShapeShapeDistance<T_SH1, T_SH2>(
+        o1, tf1, o2, tf2, nsolver, distanceRequest, distanceResult);
 
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(ConvexBase, Halfspace);
-  SHAPE_SHAPE_DISTANCE_SPECIALIZATION(TriangleP, Halfspace);
+    size_t num_contacts = 0;
+    const Vec3f& p1 = distanceResult.nearest_points[0];
+    const Vec3f& p2 = distanceResult.nearest_points[1];
+    FCL_REAL distToCollision = distance - request.security_margin;
+
+    internal::updateDistanceLowerBoundFromLeaf(request, result, distToCollision,
+                                               p1, p2);
+    if (distToCollision <= request.collision_distance_threshold &&
+        result.numContacts() < request.num_max_contacts) {
+      if (result.numContacts() < request.num_max_contacts) {
+        const Vec3f& p1 = distanceResult.nearest_points[0];
+        const Vec3f& p2 = distanceResult.nearest_points[1];
+
+        Contact contact(
+            o1, o2, distanceResult.b1, distanceResult.b2, (p1 + p2) / 2,
+            (distance <= 0 ? distanceResult.normal : (p2 - p1).normalized()),
+            -distance);
+
+        result.addContact(contact);
+      }
+      num_contacts = result.numContacts();
+    }
+
+    return num_contacts;
+  }
+};
+
+template <typename ShapeType1, typename ShapeType2>
+std::size_t ShapeShapeCollide(const CollisionGeometry* o1,
+                              const Transform3f& tf1,
+                              const CollisionGeometry* o2,
+                              const Transform3f& tf2, const GJKSolver* nsolver,
+                              const CollisionRequest& request,
+                              CollisionResult& result) {
+  return ShapeShapeCollider<ShapeType1, ShapeType2>::run(
+      o1, tf1, o2, tf2, nsolver, request, result);
+}
+
+#define SHAPE_SHAPE_DISTANCE_SPECIALIZATION(T1, T2)             \
+  template <>                                                   \
+  HPP_FCL_DLLAPI FCL_REAL ShapeShapeDistance<T1, T2>(           \
+      const CollisionGeometry* o1, const Transform3f& tf1,      \
+      const CollisionGeometry* o2, const Transform3f& tf2,      \
+      const GJKSolver* nsolver, const DistanceRequest& request, \
+      DistanceResult& result);                                  \
+  template <>                                                   \
+  HPP_FCL_DLLAPI FCL_REAL ShapeShapeDistance<T2, T1>(           \
+      const CollisionGeometry* o1, const Transform3f& tf1,      \
+      const CollisionGeometry* o2, const Transform3f& tf2,      \
+      const GJKSolver* nsolver, const DistanceRequest& request, \
+      DistanceResult& result)
+
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Box, Halfspace);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Box, Plane);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Box, Sphere);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Capsule, Capsule);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Capsule, Halfspace);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Capsule, Plane);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cone, Halfspace);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cone, Plane);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cylinder, Halfspace);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Cylinder, Plane);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere, Halfspace);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere, Plane);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere, Sphere);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(Sphere, Cylinder);
+
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(ConvexBase, Halfspace);
+SHAPE_SHAPE_DISTANCE_SPECIALIZATION(TriangleP, Halfspace);
 
 #undef SHAPE_SHAPE_DISTANCE_SPECIALIZATION
 
-#define SHAPE_SHAPE_COLLIDE_SPECIALIZATION(T1,T2) \
-template<> \
-HPP_FCL_DLLAPI std::size_t ShapeShapeCollide<T1,T2> \
-  (const CollisionGeometry* o1, const Transform3f& tf1, \
-   const CollisionGeometry* o2, const Transform3f& tf2, \
-   const GJKSolver* nsolver, const CollisionRequest& request, \
-   CollisionResult& result)
+#define SHAPE_SHAPE_COLLIDE_SPECIALIZATION(T1, T2)                         \
+  template <>                                                              \
+  struct ShapeShapeCollider<T1, T2> {                                      \
+    static HPP_FCL_DLLAPI std::size_t run(const CollisionGeometry* o1,     \
+                                          const Transform3f& tf1,          \
+                                          const CollisionGeometry* o2,     \
+                                          const Transform3f& tf2,          \
+                                          const GJKSolver* nsolver,        \
+                                          const CollisionRequest& request, \
+                                          CollisionResult& result);        \
+  }
 
-  SHAPE_SHAPE_COLLIDE_SPECIALIZATION(Sphere,Sphere);
+SHAPE_SHAPE_COLLIDE_SPECIALIZATION(Sphere, Sphere);
 
 #undef SHAPE_SHAPE_COLLIDE_SPECIALIZATION
-}
+}  // namespace fcl
 
-} // namespace hpp
+}  // namespace hpp
 
 /// @endcond
 
--- test/utility.h
+++ test/utility.h
@@ -41,62 +41,81 @@
 #include <hpp/fcl/math/transform.h>
 #include <hpp/fcl/collision_data.h>
 #include <hpp/fcl/collision_object.h>
+#include <hpp/fcl/broadphase/default_broadphase_callbacks.h>
+#include <hpp/fcl/shape/convex.h>
 
 #ifdef HPP_FCL_HAS_OCTOMAP
 #include <hpp/fcl/octree.h>
 #endif
 
 #ifdef _WIN32
-#define NOMINMAX  // required to avoid compilation errors with Visual Studio 2010
+#define NOMINMAX  // required to avoid compilation errors with Visual Studio
+                  // 2010
 #include <windows.h>
 #else
 #include <sys/time.h>
 #endif
 
-#define EIGEN_VECTOR_IS_APPROX(Va, Vb, precision)                              \
-  BOOST_CHECK_MESSAGE(((Va) - (Vb)).isZero(precision),                         \
-      "check " #Va ".isApprox(" #Vb ") failed "                                \
-      "[\n" << (Va).transpose() << "\n!=\n" << (Vb).transpose() << "\n]")
+#define EIGEN_VECTOR_IS_APPROX(Va, Vb, precision)         \
+  BOOST_CHECK_MESSAGE(((Va) - (Vb)).isZero(precision),    \
+                      "check " #Va ".isApprox(" #Vb       \
+                      ") failed "                         \
+                      "[\n"                               \
+                          << (Va).transpose() << "\n!=\n" \
+                          << (Vb).transpose() << "\n]")
 #define EIGEN_MATRIX_IS_APPROX(Va, Vb, precision)                              \
-  BOOST_CHECK_MESSAGE(((Va) - (Vb)).isZero(precision),                         \
-      "check " #Va ".isApprox(" #Vb ") failed "                                \
-      "[\n" << (Va) << "\n!=\n" << (Vb) << "\n]")
+  BOOST_CHECK_MESSAGE(((Va) - (Vb)).isZero(precision), "check " #Va            \
+                                                       ".isApprox(" #Vb        \
+                                                       ") failed "             \
+                                                       "[\n"                   \
+                                                           << (Va) << "\n!=\n" \
+                                                           << (Vb) << "\n]")
 
-namespace octomap{
+namespace octomap {
 #ifdef HPP_FCL_HAS_OCTOMAP
 typedef hpp::fcl::shared_ptr<octomap::OcTree> OcTreePtr_t;
 #endif
-}
-namespace hpp
-{
-namespace fcl
-{
+}  // namespace octomap
+namespace hpp {
+namespace fcl {
 
-class BenchTimer
-{
-public:
+class BenchTimer {
+ public:
   BenchTimer();
   ~BenchTimer();
 
-  void start();                               ///< start timer
-  void stop();                                ///< stop the timer
-  double getElapsedTime();                    ///< get elapsed time in milli-second
-  double getElapsedTimeInSec();               ///< get elapsed time in second (same as getElapsedTime)
-  double getElapsedTimeInMilliSec();          ///< get elapsed time in milli-second
-  double getElapsedTimeInMicroSec();          ///< get elapsed time in micro-second
+  void start();                       ///< start timer
+  void stop();                        ///< stop the timer
+  double getElapsedTime();            ///< get elapsed time in milli-second
+  double getElapsedTimeInSec();       ///< get elapsed time in second (same as
+                                      ///< getElapsedTime)
+  double getElapsedTimeInMilliSec();  ///< get elapsed time in milli-second
+  double getElapsedTimeInMicroSec();  ///< get elapsed time in micro-second
 
-private:
-  double startTimeInMicroSec;                 ///< starting time in micro-second
-  double endTimeInMicroSec;                   ///< ending time in micro-second
-  int stopped;                                ///< stop flag
+ private:
+  double startTimeInMicroSec;  ///< starting time in micro-second
+  double endTimeInMicroSec;    ///< ending time in micro-second
+  int stopped;                 ///< stop flag
 #ifdef _WIN32
-  LARGE_INTEGER frequency;                    ///< ticks per second
+  LARGE_INTEGER frequency;  ///< ticks per second
   LARGE_INTEGER startCount;
   LARGE_INTEGER endCount;
 #else
   timeval startCount;
   timeval endCount;
 #endif
+};
+
+struct TStruct {
+  std::vector<double> records;
+  double overall_time;
+
+  TStruct() { overall_time = 0; }
+
+  void push_back(double t) {
+    records.push_back(t);
+    overall_time += t;
+  }
 };
 
 extern const Eigen::IOFormat vfmt;
@@ -107,87 +126,82 @@ extern const Vec3f UnitY;
 extern const Vec3f UnitZ;
 
 /// @brief Load an obj mesh file
-void loadOBJFile(const char* filename, std::vector<Vec3f>& points, std::vector<Triangle>& triangles);
+void loadOBJFile(const char* filename, std::vector<Vec3f>& points,
+                 std::vector<Triangle>& triangles);
 
-void saveOBJFile(const char* filename, std::vector<Vec3f>& points, std::vector<Triangle>& triangles);
+void saveOBJFile(const char* filename, std::vector<Vec3f>& points,
+                 std::vector<Triangle>& triangles);
 
 #ifdef HPP_FCL_HAS_OCTOMAP
-fcl::OcTree loadOctreeFile (const std::string& filename, const FCL_REAL& resolution);
+fcl::OcTree loadOctreeFile(const std::string& filename,
+                           const FCL_REAL& resolution);
 #endif
 
-/// @brief Generate one random transform whose translation is constrained by extents and rotation without constraints. 
-/// The translation is (x, y, z), and extents[0] <= x <= extents[3], extents[1] <= y <= extents[4], extents[2] <= z <= extents[5]
+/// @brief Generate one random transform whose translation is constrained by
+/// extents and rotation without constraints. The translation is (x, y, z), and
+/// extents[0] <= x <= extents[3], extents[1] <= y <= extents[4], extents[2] <=
+/// z <= extents[5]
 void generateRandomTransform(FCL_REAL extents[6], Transform3f& transform);
 
-/// @brief Generate n random transforms whose translations are constrained by extents.
-void generateRandomTransforms(FCL_REAL extents[6], std::vector<Transform3f>& transforms, std::size_t n);
+/// @brief Generate n random transforms whose translations are constrained by
+/// extents.
+void generateRandomTransforms(FCL_REAL extents[6],
+                              std::vector<Transform3f>& transforms,
+                              std::size_t n);
 
-/// @brief Generate n random transforms whose translations are constrained by extents. Also generate another transforms2 which have additional random translation & rotation to the transforms generated.
-void generateRandomTransforms(FCL_REAL extents[6], FCL_REAL delta_trans[3], FCL_REAL delta_rot, std::vector<Transform3f>& transforms, std::vector<Transform3f>& transforms2, std::size_t n);
+/// @brief Generate n random transforms whose translations are constrained by
+/// extents. Also generate another transforms2 which have additional random
+/// translation & rotation to the transforms generated.
+void generateRandomTransforms(FCL_REAL extents[6], FCL_REAL delta_trans[3],
+                              FCL_REAL delta_rot,
+                              std::vector<Transform3f>& transforms,
+                              std::vector<Transform3f>& transforms2,
+                              std::size_t n);
 
-/// @ brief Structure for minimum distance between two meshes and the corresponding nearest point pair
-struct DistanceRes
-{
+/// @ brief Structure for minimum distance between two meshes and the
+/// corresponding nearest point pair
+struct DistanceRes {
   double distance;
   Vec3f p1;
   Vec3f p2;
 };
 
-/// @brief Collision data stores the collision request and the result given by collision algorithm. 
-struct CollisionData
-{
-CollisionData() : request (NO_REQUEST, 1)
-  {
-    done = false;
-  }
+/// @brief Default collision callback for two objects o1 and o2 in broad phase.
+/// return value means whether the broad phase can stop now.
+bool defaultCollisionFunction(CollisionObject* o1, CollisionObject* o2,
+                              void* cdata);
 
-  /// @brief Collision request
-  CollisionRequest request;
-
-  /// @brief Collision result
-  CollisionResult result;
-
-  /// @brief Whether the collision iteration can stop
-  bool done;
-};
-
-
-/// @brief Distance data stores the distance request and the result given by distance algorithm. 
-struct DistanceData
-{
-  DistanceData()
-  {
-    done = false;
-  }
-
-  /// @brief Distance request
-  DistanceRequest request;
-
-  /// @brief Distance result
-  DistanceResult result;
-
-  /// @brief Whether the distance iteration can stop
-  bool done;
-
-};
-
-/// @brief Default collision callback for two objects o1 and o2 in broad phase. return value means whether the broad phase can stop now.
-bool defaultCollisionFunction(CollisionObject* o1, CollisionObject* o2, void* cdata);
-
-/// @brief Default distance callback for two objects o1 and o2 in broad phase. return value means whether the broad phase can stop now. also return dist, i.e. the bmin distance till now
-bool defaultDistanceFunction(CollisionObject* o1, CollisionObject* o2, void* cdata, FCL_REAL& dist);
+/// @brief Default distance callback for two objects o1 and o2 in broad phase.
+/// return value means whether the broad phase can stop now. also return dist,
+/// i.e. the bmin distance till now
+bool defaultDistanceFunction(CollisionObject* o1, CollisionObject* o2,
+                             void* cdata, FCL_REAL& dist);
 
 std::string getNodeTypeName(NODE_TYPE node_type);
 
 Quaternion3f makeQuat(FCL_REAL w, FCL_REAL x, FCL_REAL y, FCL_REAL z);
 
-std::ostream& operator<< (std::ostream& os, const Transform3f& tf);
+std::ostream& operator<<(std::ostream& os, const Transform3f& tf);
 
 /// Get the argument --nb-run from argv
-std::size_t getNbRun (const int& argc, char const* const* argv, std::size_t defaultValue);
+std::size_t getNbRun(const int& argc, char const* const* argv,
+                     std::size_t defaultValue);
 
-}
+void generateEnvironments(std::vector<CollisionObject*>& env,
+                          FCL_REAL env_scale, std::size_t n);
 
-} // namespace hpp
+void generateEnvironmentsMesh(std::vector<CollisionObject*>& env,
+                              FCL_REAL env_scale, std::size_t n);
+
+/// @brief We give an ellipsoid as input. The output is a 20 faces polytope
+/// which vertices belong to the original ellipsoid surface. The procedure is
+/// simple: we construct a icosahedron, see
+/// https://sinestesia.co/blog/tutorials/python-icospheres/ . We then apply an
+/// ellipsoid tranformation to each vertex of the icosahedron.
+Convex<Triangle> constructPolytopeFromEllipsoid(const Ellipsoid& ellipsoid);
+
+}  // namespace fcl
+
+}  // namespace hpp
 
 #endif
